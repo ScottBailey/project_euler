15d7733b59c336fd57928261448b3dd9b5a340b5
Scott Bailey <sbailey@inter-coastal.net>
2021-07-30 20:57:12 -0700

    63

---
+++ p0063/src/main.rs
@@ -0,0 +1,91 @@
+/*
+
+https://projecteuler.net
+
+The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the
+9-digit number, 134217728=8^9, is a ninth power.
+
+How many n-digit positive integers exist which are also an nth power?
+
+*/
+
+
+fn digit_count(mut n : u128) ->u128 {
+
+    let mut rv = 0;
+    while n > 0 {
+        rv += 1;
+        n /= 10;
+    }
+
+    rv
+}
+
+
+fn pow(x :u128, y :u128) -> u128 {
+
+    if y == 0 {
+        return 1;
+    }
+    let mut rv = x;
+    for _ in 2..=y {
+        rv *= x;
+    }
+
+    rv
+}
+
+
+
+fn solve() -> u128 {
+
+    let mut rv = 0;
+
+    for y in 1.. {
+        let mut found = false;
+        for x in 1.. {
+            let n = pow(x,y);
+            let c = digit_count(n);
+            if c == y {
+                rv += 1;
+                found = true;
+            }
+            if c > y {
+                break;
+            }
+        }
+        if !found {
+            break;
+        }
+    }
+
+    rv
+}
+
+
+fn main() {
+
+    let start_time = std::time::Instant::now();
+
+    let sol = solve();
+
+    let elapsed = start_time.elapsed().as_micros();
+    println!("\nSolution: {}", sol);
+
+    let mut remain = elapsed;
+    let mut s = String::new();
+    if remain == 0 {
+        s.insert(0,'0');
+    }
+    while remain > 0 {
+        let temp = remain%1000;
+        remain /= 1000;
+        if remain > 0 {
+            s = format!(",{:03}",temp) + &s;
+        }
+        else {
+            s = format!("{}",temp) + &s;
+        }
+    }
+    println!("Elasped time: {} us", s);
+}
